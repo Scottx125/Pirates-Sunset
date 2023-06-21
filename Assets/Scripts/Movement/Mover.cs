947325c7940d26b447a6b8a0b796034719fdb656
Scottx125 <31070452+Scottx125@users.noreply.github.com>
2023-06-21 14:33:33 +0100

    V 0.0.2.1
    Tweaking around with physics materials. Modified Mover script, utilising floats as fixed stated didn't feel right, so I made a custom struct, stored the name of the sail state and modifier float value inside that. Then used int values to access an array in which these states are stored.

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PirateGame.Movement{
@@ -5,7 +6,8 @@ namespace PirateGame.Movement{
     public class Mover : MonoBehaviour
     {
         // States of control.
-        private float sailState;
+        private float sailStateModifier;
+        private int sailState = 0;
 
         // State change delay.
         [SerializeField]
@@ -20,6 +22,28 @@ namespace PirateGame.Movement{
         [SerializeField]
         private float acceleration;
         Vector3 velocity, desiredVelocity;
+
+        // Sail struct, names and float modifier values stored in a struct and held in an array.
+        private struct SailStateStruct{
+            private string sailStateName;
+            private float sailStateSpeedModifier;
+
+            public string getSailStateName => sailStateName;
+            public float getSailStateSpeedModifier => sailStateSpeedModifier;
+
+            public SailStateStruct(string sailStateEnum, float sailStateSpeedModifier){
+                this.sailStateName = sailStateEnum;
+                this.sailStateSpeedModifier = sailStateSpeedModifier;
+            }
+        }
+
+        private SailStateStruct[] sailStateArray = new SailStateStruct[5]{
+            new SailStateStruct("Reefed Sail", 0f),
+            new SailStateStruct("Quater Sail", 0.25f),
+            new SailStateStruct("Half Sail", 0.5f),
+            new SailStateStruct("Three Quaters Sail", 0.75f),
+            new SailStateStruct("Full Sail", 1f),
+        };
 
         // Other Scripts/Componenets
         Rigidbody rb;
@@ -56,14 +80,14 @@ namespace PirateGame.Movement{
         {
             
             // Set desired speed based on sailstate and input.
-            float sailSpeedImpact = maxSpeed * sailState;
+            float sailSpeedImpact = maxSpeed * sailStateModifier;
             desiredVelocity = new Vector3(0f,0f, sailSpeedImpact);
 
             // Velocity = current rb velocity.
             velocity = rb.velocity;
 
             // Rate of acceleration.
-            float maxSpeedChange = (acceleration + sailState) * Time.deltaTime;
+            float maxSpeedChange = (acceleration + sailStateModifier) * Time.deltaTime;
 
             // Moves the velocity values towards the desiredVelocity at the max speed change.
 		    velocity.x =
@@ -77,15 +101,17 @@ namespace PirateGame.Movement{
         }
 
         public void SailStateIncrease(){
-            if (sailState < 1f && sailStateTimeSinceChanged >= sailStateChangeDelay){
-                sailState += .25f;
-                sailStateTimeSinceChanged = 0f; 
+            if (sailState < 4 && sailStateTimeSinceChanged >= sailStateChangeDelay){
+                sailState++;
+                sailStateModifier = sailStateArray[sailState].getSailStateSpeedModifier;
+                sailStateTimeSinceChanged = 0f;
             }
         }
         public void SailStateDecrease(){
-            if (sailState > 0f && sailStateTimeSinceChanged >= sailStateChangeDelay){
-                sailState -= .25f;
-                sailStateTimeSinceChanged = 0f; 
+            if (sailState > 0 && sailStateTimeSinceChanged >= sailStateChangeDelay){
+                sailState--;
+                sailStateModifier = sailStateArray[sailState].getSailStateSpeedModifier;
+                sailStateTimeSinceChanged = 0f;
             }
         }
     }
