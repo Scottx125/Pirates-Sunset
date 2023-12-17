4e6ede2fb2a6a85000aa82ad306f1c9738c19a36
Scottx125 <31070452+Scottx125@users.noreply.github.com>
2023-12-17 22:56:44 +0000

    1.3.1
    ---ADDITIONS---
    N/A
    ---FIXES---
    N/A
    ---PROBLEMS---
    N/A
    ---REMOVED---
    N/A
    ---COMMENTS---
    Fleshing out the abstract base for the Inventory Object.

--- Assets/Scripts/Inventory/InventoryObject.cs
+++ Assets/Scripts/Inventory/InventoryObject.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public abstract class InventoryObject : MonoBehaviour
 {
-
-    public int Quantity { get { return quantity; }}
     [SerializeField]
     protected bool bIsActivateable = false;
     [SerializeField]
@@ -13,11 +11,52 @@ public abstract class InventoryObject : MonoBehaviour
     [SerializeField]
     protected float activeTime = 1f;
     [SerializeField]
+    protected float cooldown = 1f;
+    [SerializeField]
     protected float repeatTime = 0.25f;
+    [SerializeField]
+    protected int quantity = 0;
 
 
     protected bool bIsActive = false;
-    protected int quantity = 0;
 
-    public abstract void InventoryObjectBehaviour();
+    public void CheckBehaviour()
+    {
+        // Ensure object is activateable and is not currently active.
+        if (bIsActivateable == false) return;
+        if (bIsActive == true) return;
+        // Begin coroutine.
+        StartCoroutine(InitiateBehaviour());
+    }
+    private IEnumerator InitiateBehaviour()
+    {
+        // Stops a new coroutine being fired and sets up variables.
+        bIsActive = true;
+        float endTime = Time.time + activeTime;
+        // Handles repeat behaviours.
+        if (repeatBehaviour == true)
+        {
+            while (Time.time < endTime)
+            {
+                ObjectBehaviour();
+                yield return new WaitForSeconds(repeatTime);
+            }
+        } else
+        {
+            // Single-Fire behaviours.
+            ObjectBehaviour();
+        }
+        // Waits for the cooldown to expire and then exists, fully resetting the behaviour.
+        yield return new WaitForSeconds(cooldown);
+        bIsActive = false;
+    }
+
+    protected virtual void ObjectBehaviour()
+    {
+        // Implement in child classes.
+    }
+
+    public int GetQuantity() { return quantity; }
+    public void SubtractQuantity(int subtractValue) { quantity -= subtractValue; }
+    public void AddQuantity(int addValue) { quantity += addValue; }
 }
